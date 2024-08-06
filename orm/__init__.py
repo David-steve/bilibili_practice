da03a980c068d966a4e24420927b15ba8762cf73
laijinwei <18776666967@qq.com>
2024-08-06 22:50:06 +0800

    feature: 补充init 文件

--- orm/__init__.py
+++ orm/__init__.py
@@ -3,7 +3,3 @@ import os
 import pymysql
 
 pymysql.install_as_MySQLdb()
-
-os.environ[
-    "ENV"] = '{"db_host": "110.41.145.76", "db_port": "3306", "db_user": "root",' \
-             ' "db_password": "mysql@2024", "db_name": "mixed"}'
