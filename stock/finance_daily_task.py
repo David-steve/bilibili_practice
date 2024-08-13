a2fad327372bda961ffb44b06de2000e88eda63f
laijinwei <18776666967@qq.com>
2024-08-13 16:44:27 +0800

    feature: 调试

--- stock/finance_daily_task.py
+++ stock/finance_daily_task.py
@@ -1,8 +1,6 @@
-from env_init import init
 from history_gold_price_service import get_yesterday_gold_price
 from stock_service import get_today_stock_info
 
-init()
 
 if __name__ == '__main__':
     get_yesterday_gold_price()
