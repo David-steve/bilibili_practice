abc018439f16140d7809e927ada40cf3664d9525
laijinwei <18776666967@qq.com>
2024-08-21 13:29:01 +0800

    feature: 抽取cookie, 降低耦合度

--- stock/stock_service.py
+++ stock/stock_service.py
@@ -1,5 +1,6 @@
 from typing import Generator, Any, Dict
 
+from Bili.db.models import Cookies
 from Bili.util.utils import week_day, curdate
 
 import requests
@@ -23,12 +24,13 @@ def get_stock_info(stock_symbol: str = '', begin_date: str = '2024-08-06', count
 
     print(querystring)
     # exit()
+    cookie:Cookies = Cookies.objects.filter(website='雪球').first()
 
     headers = {
         "origin": "https://xueqiu.com",
         "referer": "https://xueqiu.com/S/SZ002594",
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
-        "Cookie": "cookiesu=831720582869863; device_id=670925312d536e47f413d7e647af2023; xq_a_token=fb0f503ef881090db449e976c330f1f2d626c371; xqat=fb0f503ef881090db449e976c330f1f2d626c371; xq_r_token=967c806e113fbcb1e314d5ef2dc20f1dd8e66be3; xq_id_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1aWQiOi0xLCJpc3MiOiJ1YyIsImV4cCI6MTcyNTMyNDc3NSwiY3RtIjoxNzIyODU4NzU5NjAwLCJjaWQiOiJkOWQwbjRBWnVwIn0.E_Ch7OjrIJtkRffEgs86xdumMK0dOtFS6rfTLhdVOrH_z-oSpEHnVnjP72F5EUXAogqxnj5k2dADeK8KPex6gOAcgsmauWGOZfulDYaIuBb4zSKKLwMMWd6FyHJlWc4OolEEHSVWG6P7AHqmkIIoL93447kTl6pFplTeRpcQKbsofPrP9Ijwz_-XGNrdPdLdIR_MbZJACIYEjbkU1DmqMYn_3i-Q52KTlkrdgbAWYBuRCUUgpGPvT9kiIF37ToZz75tkMlP-HoG7B7pZktqiGkC8D0BDPOKBtL-6OiDbR9gPilQSRol45rCW805WwYhAeB19f9AGMqDRFbGniprsXg; u=831720582869863; Hm_lvt_1db88642e346389874251b5a1eded6e3=1720582881,1722858779; HMACCOUNT=5C9D722CF5E66B86; s=cg1203yzea; Hm_lpvt_1db88642e346389874251b5a1eded6e3=1722909422; ssxmod_itna=eqAxR7G=0=qWq7K50dq0PDQFuCjiDgGxbQDGqeery4F5DsNTDSxGKidDqxBnn=hoHK070xK5tzOeb5iqwho7Qv7zrGnLfUK0e0aDbqGkiBQY2eDx6q0rD74irDDxD3Db3QDSDWKD9D0bCy65suxGWDmbCDWPDYxDr26KDRxi7DDH=sx07DQHCd7e17DDz773KemwoApAwsoEz8+5ett0D2xG1C40HtjdK2OoX/+xGtbL65/R8DlKuDCKsSP7788x149yhai0dKj+iIQrbC14o7Dr4LmtHEL04GQ25eYhx=i2Dan2e7DDWqGhloUxYD==; ssxmod_itna2=eqAxR7G=0=qWq7K50dq0PDQFuCjiDgGxbQDGqeery4KikUbooDl=7iDj+hoFzSq5qjpqnNqnQmk2r+t8fkGNYYcOQP6egKQAEE63E8d8H2qGmMomhvycCG2EYdmk3e8SlaGD6XD8oQiY40sBThClMdY0GK5/qvsUdxnDjQmCioDpLrslwt2mDrIeMiVn2QpB3bl9x=4fx=o+4e5b35PHGK3qqo31ciedkA1TL9UlQ54RZBPH+6G2CdQBTdNXStwlT6gSW=9t/WwuWrKacTdOcu=ElmsQtBLKfTIQi0tamNVAlWKrI0bAxh/HVtIbDYh9e4VrViT7Uqh4WDM+bIxrS+q7Wz/xk74RG0R7yfnAxCi6WA7nH1Fa3PK=nuFQhocR=97DHTh3WDH0HCGF0+sDxNZIe=0PkEvCGtYnDIn130BBv=8wsbD/8WcW24TRTlnEiRstc14g8jhFjTbKds4p5uauFH9CAUfKXjrv4uiWA5RPR4T/WR1pBFn3ek4hro8i5RS4ClL3HWDaROTgWeeduoRI4zTaRSHmgCDcBx4BHIRzrwNY0qN1RbGdD07eDqyAC/Bq/A5No9uFfdoxKequCxma3g7KZadcq5t7O12BP8IRUUP5ew4wMAfIGyoGSUTN7GkPsbUuadGghNFrAYqDDLxD20GDD==="
+        "Cookie": cookie.cookie_value
     }
 
     return requests.request("GET", url, headers=headers, params=querystring)
